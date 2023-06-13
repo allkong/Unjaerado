ec3a0aac5549ba3277a354a496e8376b769d463f
allempti <115684201+allempti@users.noreply.github.com>
2023-06-13 15:26:06 +0900

    feature home
    - 헤더 구현
    - 홈 화면 구현
    - 로그인, 로그아웃 예시

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+function handleRoute($url) {
+    switch ($url) {
+        default:
+            include 'pages/home.php';
+            break;
+        case 'product':
+            include 'pages/product.php'; 
+            break;
+        case '':
+            include 'pages/product.php'; 
+            break;
+        case 'community':
+            include 'pages/community.php'; 
+            break;
+        case 'challenge':
+            include 'pages/challenge.php';
+            break;
+    }
+}
+
+// URL 파라미터에서 요청된 경로 가져오기
+$url = isset($_GET['url']) ? $_GET['url'] : '';
+
+// 라우팅 처리할 함수를 호출합
+handleRoute($url);
